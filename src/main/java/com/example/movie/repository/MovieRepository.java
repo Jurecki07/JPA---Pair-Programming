56d64effac358ce6fbd8342d8cb602b5abe5b810
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-02 11:00:07 -0500

    repo

---
+++ src/main/java/com/example/movie/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.example.movie.repository;
+
+import com.example.movie.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+
+}
