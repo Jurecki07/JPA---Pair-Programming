d8104bf342c0dc40ac7ea56facc4852a41351d04
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-02 10:56:56 -0500

    setting hash code

--- src/main/java/com/example/movie/domain/Movie.java
+++ src/main/java/com/example/movie/domain/Movie.java
@@ -1,7 +1,6 @@
 package com.example.movie.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
@@ -11,49 +10,65 @@ import java.time.LocalDate;
 
 public class Movie {
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        return id == movie.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
     private int id;
     private String title;
     private LocalDate releaseDate;
     private Genre genre;
     private boolean color;
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int getId() {
-        return id;
-    }
+        return id; }
+
+
 
     public void setId(int id) {
         this.id = id;
     }
 
     public String getTitle() {
-        return title;
-    }
+        return title; }
+
 
     public void setTitle(String title) {
-        this.title = title;
-    }
+        this.title = title; }
 
+
+@Column(name = "release_date")
     public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
+        return releaseDate; }
 
     public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
+        this.releaseDate = releaseDate; }
 
     public Genre getGenre() {
-        return genre;
-    }
+        return genre; }
 
     public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
+        this.genre = genre; }
+
 
     public boolean isColor() {
-        return color;
-    }
+        return color; }
+
 
     public void setColor(boolean color) {
-        this.color = color;
-    }
+        this.color = color; }
 }
