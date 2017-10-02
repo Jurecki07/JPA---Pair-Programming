2ef22abb73d866af33af0e2c8ca99c904a3c6b68
Tyler Jurecki <jurecki07@yahoo.com>
2017-10-02 11:24:10 -0500

    Mapping

--- src/main/java/com/example/movie/controller/MovieController.java
+++ src/main/java/com/example/movie/controller/MovieController.java
@@ -1,10 +1,32 @@
 package com.example.movie.controller;
 
 
+import com.example.movie.domain.Movie;
+import com.example.movie.service.MovieServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
+
+    @Autowired
+    MovieServiceImpl movieService;
+
+
+    @GetMapping("/movies")
+    public List<Movie> getMovies(){
+        return movieService.get();
+    }
+
+    @PostMapping("/movies")
+        public Movie addMovie(){
+        movieService.add(movie)
+
+    }
 
 
 }
